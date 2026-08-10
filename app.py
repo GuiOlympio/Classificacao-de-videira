e0a20e982a8db67b8b31cb0cbeb02427fd8e7d8f
Guilherme Olympio <ginguiolympio@gmail.com>
2026-08-10 12:59:13 +0000

    commit

--- app.py
+++ app.py
@@ -1,1 +1,59 @@
 import streamlit as st
+import gdown
+import tensorflow as tf
+import io
+from PIL import Image
+import numpy as np
+
+
+def carrega_modelo():
+  url= "Google drive ir la pega"
+  gdown.download(url,"modelo_quantizado.tflite")
+  interpreter = tf.lite.Interpreter(modelo_path = "modelo_quantizado.tflite")
+  interpreter.allocate_tensors()
+  return interpreter
+
+def carrega_imagem():
+  upload_file = st.file_uploader("Arraste e solte uma imagem aqui ou clique para selecionar uma", type= ['png','jpg', 'jpeg'])
+
+  if upload_file is not None:
+    image_data = uploaded_file.read()
+    image = Image.open(io.BytesIO(image_data))
+
+    st.image(image)
+    st.success('Imagem foi carregada com sucesso')
+
+    image = np.array(image, dtype = np.float32)
+    image = image / 255.0
+    image = np.expand.dims(image, axis = 0)
+
+    return image
+
+def main():
+
+  st.set_page_config(
+    page_title = "Classissifica folhas de videira",
+    page_icon = "🍃",
+  )
+
+  st.write("# Classissifica folhas de videira!!")
+  
+  #Carrega modelo
+  interpreter = carrega_modelo()
+  #carrega imagem
+  imagem = carrega_imagem()
+
+  if image is not None:
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], image)
+    interpreter.invoke()
+
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    predicted_class = np.argmax(output_data)
+
+    st.write(f"A classe prevista é: {predicted_class}")
+  # Classifica
+  if __name__ == "__main__":
+    main()
