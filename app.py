d8eedf1ab82b3c7ed24bccc34e4269b952cfc320
Guilherme Olympio <ginguiolympio@gmail.com>
2026-08-11 04:14:06 +0000

    commit

--- app.py
+++ app.py
@@ -7,71 +7,8 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
-@st.cache_resource
-def carrega_modelo():
-  
-  url= "https://colab.research.google.com/drive/1NiHVForsc3zOQ2_kr3yKXYgSujp008IA?usp=drive_link"
-  gdown.download(url,"modelo_quantizado.tflite")
-  interpreter = tf.lite.Interpreter(modelo_path = "modelo_quantizado.tflite")
-  interpreter.allocate_tensors()
-  return interpreter
-
-def carrega_imagem():
-  upload_file = st.file_uploader("Arraste e solte uma imagem aqui ou clique para selecionar uma", type= ['png','jpg', 'jpeg'])
-
-  if upload_file is not None:
-    image_data = uploaded_file.read()
-    image = Image.open(io.BytesIO(image_data))
-
-    st.image(image)
-    st.success('Imagem foi carregada com sucesso')
-
-    image = np.array(image, dtype = np.float32)
-    image = image / 255.0
-    image = np.expand.dims(image, axis = 0)
-
-    return image
-
 
 def previsao(interpreter, image):
-  input_details = interpreter.get_input_details()
-  output_details = interpreter.get_output_details()
-
-  interpreter.set_tensor(input_details[0]['index'],image)
-
-  interpreter.invoke()
-
-  output_data = interpreter.get_tensor(output_details[0]['index'])
-  classes = ['LeafBlight', 'BlackRot', 'BlackMeasles', 'HealthyGrapes']
-
-  df = pd.DataFrame()
-  de['classes'] = classes
-  df['probabilidades(%)'] = 100*output_data[0]
-
-  fig = px.bar(df,
-               y='classes', 
-               x='probabilidades (%)', 
-               orientation ='h', 
-               text = 'probabilidades (%) de doenças em Uvas',)
-  st.plotly_chart(fig)
- 
-
-
-def main():
-
-  st.set_page_config(
-    page_title = "Classissifica folhas de videira",
-    page_icon = "🍃",
-  )
-
-  st.write("# Classissifica folhas de videira!!")
-  
-  #Carrega modelo
-  interpreter = carrega_modelo()
-  #carrega imagem
-  image = carrega_imagem()
-
-  if image is not None:
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
@@ -79,9 +16,83 @@ def main():
     interpreter.invoke()
 
     output_data = interpreter.get_tensor(output_details[0]['index'])
-    predicted_class = np.argmax(output_data)
+    classes = ['BlackMeasles', 'BlackRot', 'HealthyGrapes', 'LeafBlight']
 
-    st.write(f"A classe prevista é: {predicted_class}")
-  # Classifica
-  if __name__ == "__main__":
+    # Correção: DataFrame com 'F' maiúsculo
+    df = pd.DataFrame()
+    df['classes'] = classes
+    df['probabilidade (%)'] = 100 * output_data[0]
+
+    fig = px.bar(
+        df, 
+        y='classes', 
+        x='probabilidade (%)', 
+        orientation='h', 
+        text='probabilidade (%)',
+        title='Probabilidade de Classes de Doenças de Uva'
+    )
+
+    st.plotly_chart(fig)
+
+
+def carrega_modelo():
+    # Substitua pelo ID do arquivo do modelo hospedado no Google Drive:
+    # Exemplo: url = 'https://drive.google.com/uc?id=1234567890abc...'
+    url = "https://colab.research.google.com/drive/1NiHVForsc3zOQ2_kr3yKXYgSujp008IA?usp=drive_link"
+    
+    gdown.download(url, "modelo_quantizado.tflite", quiet=False)
+    
+    # Correção: model_path (em inglês)
+    interpreter = tf.lite.Interpreter(model_path="modelo_quantizado.tflite")
+    interpreter.allocate_tensors()
+    return interpreter
+
+
+def carrega_imagem():
+    upload_file = st.file_uploader(
+        "Arraste e solte uma imagem aqui ou clique para selecionar uma", 
+        type=['png', 'jpg', 'jpeg']
+    )
+
+    if upload_file is not None:
+        # Correção: padronização para upload_file
+        image_data = upload_file.read()
+        image = Image.open(io.BytesIO(image_data))
+
+        # Redimensionar para a entrada esperada pelo modelo TFLite (256x256 por padrão)
+        image = image.resize((256, 256))
+
+        st.image(image, caption="Imagem Carregada")
+        st.success('Imagem foi carregada com sucesso!')
+
+        image = np.array(image, dtype=np.float32)
+        image = image / 255.0
+        image = np.expand.dims(image, axis=0)
+
+        return image
+    return None
+
+
+def main():
+    st.set_page_config(
+        page_title="Classifica folhas de videira",
+        page_icon="🍃",
+    )
+
+    st.write("# Classifica folhas de videira!!")
+    
+    # Carrega modelo
+    interpreter = carrega_modelo()
+    
+    # Carrega imagem
+    image = carrega_imagem()
+
+    # Classifica
+    if image is not None:
+        # Correção: passando os dois parâmetros corretamente
+        previsao(interpreter, image)
+
+
+# Correção: bloco fora da função main()
+if __name__ == "__main__":
     main()
